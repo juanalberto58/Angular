c068c9b7dab51db269f93a0c77da51d1895779bd
juanalberto58 <juanalberto580@gmail.com>
2020-12-19 16:19:13 +0100

    Añadiendo pequeño componente User,closes #3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './components/app/app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { UserComponent } from './components/user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/user/user.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.sass']
+})
+export class UserComponent{
+
+  constructor() { }
+
+  isLogged: boolean = false
+}
