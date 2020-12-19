5b1b1bddff66408a8940f82d5b91f9e286e7d925
juanalberto58 <juanalberto580@gmail.com>
2020-12-19 14:12:43 +0100

    Añadiendo OnInit en clase Header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.sass']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
 
+
+  /*Constructor*/
   constructor() {
-    console.log("Component Header Creado!")
+    console.log("Component Header Creandose!")
+   }
+
+  /*Se ejecuta una vez que se ha creado el componente satisfactoriamente*/
+   ngOnInit():void {
+    console.log("Component Header ya creado!")
    }
 
 }
